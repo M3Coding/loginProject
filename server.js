e4a5a0ab9a6f8e93c9cf815f2dd0092ebe2a2952
M3Coding <meady2009@gmail.com>
2025-09-11 13:59:32 -0400

    add passport authentication

--- server.js
+++ server.js
@@ -2,16 +2,39 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import pg from "pg";
 import bcrypt from "bcrypt";
+import "dotenv/config";
+import session from "express-session";
+import passport from "passport";
+import {Strategy} from "passport-local";
 
+
+
+dotenv.config({ path: '/.env' })
 const app = express();
 const port = 3000;
 const saltRounds = 10;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(session({
+    secret: process.env.secret,
+    resave: false,
+    saveUnititialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24,
+    }
+})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 const db = new pg.Client( {
     database: "testUsers",
     host: "localhost",
     port: 5432,
     user: "postgres",
-    password: "CodingGenius2023",
+    password: process.env.dbPassword,
 
 });
 db.connect();
@@ -25,27 +48,7 @@ const insert = (fname, lname, username, email, password) => {
 } //insert into db and add hash function for password
 
 let verification = async(user, pass) => {
-    try{
-    let result = await db.query("SELECT * FROM users WHERE username=$1", [user]);
-    let userInfo = result.rows[0];
-    console.log(userInfo);
-    if (!userInfo) {
-        console.log("User not found!");
-        return false; 
-    } 
-    const match = await bcrypt.compare(pass, userInfo.password);
-    if (match) {
-        console.log("Login Successful")
-        return true;
-    } else {
-        console.log("Login Unsuccessful")
-        return false;
-    }
-
-    }catch(err) {
-        console.error("User Verification failed:", err);
-        return false;
-    }
+    
  }
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -53,7 +56,14 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    console.log(req.user);//check to see if user is authenticated.
+    if(req.isAuthenticated()) {
+        res.render("index.ejs");
+    } else {
+        res.render("/login")
+
+    }
+    ;
 })
 
 //Register Server
@@ -72,17 +82,42 @@ app.post("/submitRegister", (req, res) => {
     res.render("register.ejs")
 })
 //Login Server
-app.post("/submitLogin", (req, res) => {
-    let userInput = req.body.username;
-    let userPass = req.body.password;
+app.post("/submitLogin", passport.authenticate("local", { 
+    successRedirect:"/",
+    failureRedirect:"/login",
 
-verification(userInput, userPass);
-//check if the login information stored is matching what the user is putting in. Hardcoded object with login information. 
-res.redirect("/")
-})
+}));
 
+//Triggers every time it tries to authenticate user
+passport.use(new Strategy(async function verify(username, password, cb) {
+    try{
+    let result = await db.query("SELECT * FROM users WHERE username=$1", [username]);
+    let userInfo = result.rows[0];
+    console.log(userInfo);
+    if (!userInfo) {
+        console.log("User not found!");
+        return false; 
+    } 
+    const match = await bcrypt.compare(pass, userInfo.password);
+    if (match) {
+        return cb(null, user)
+    } else {
+        return cb(null, user)
+    }
+
+    }catch(err) {
+        return cb("User Verification failed:", err);
+    }; //check if the login information stored is matching what the user is putting in. Hardcoded object with login information. 
+}))
 //Next steps. add postgres database to recieve registration and check it with login screen. 
 
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+});//save users information in local session for access
+
+passport.deserializeUser((user, cb) => {
+    cb(null, user)
+});//to get user it access user information through that session 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
